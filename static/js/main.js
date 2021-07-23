aa31fdb072902d6f2fc1bcb7cb5ab2b8a1990fa2
troll92and <troll92and@gmail.com>
2021-07-23 13:44:07 +0200

    try thr wep api

--- static/js/main.js
+++ static/js/main.js
@@ -4,4 +4,15 @@ function init() {
   boardsManager.loadBoards()
 }
 
+async function registerSW() {
+  if ('serviceWorker' in navigator) {
+    try {
+      await navigator.serviceWorker.register('./static/js/sw.js');
+    } catch (e) {
+      console.log('SW registration failed');
+    }
+  }
+}
+
 init();
+registerSW();
